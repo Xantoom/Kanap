bea33df14fe3efe3a12cace61b80595efa0d3a97
Xavier Lauer <xantoom@gmail.com>
2023-06-02 14:50:40 +0200

    Récupération des données d'un produit et affichage de ces données

---
+++ front/js/product.js
@@ -0,0 +1,47 @@
+const productId = new URLSearchParams(window.location.search).get("id");
+
+if (!productId) window.alert("Aucun produit sélectionné");
+
+/**
+ * Récupère un produit depuis l'API et l'affiche
+ */
+fetchFromApi(productId).then((product) => {
+    // Affichage du produit
+    document.querySelector(".item__img").innerHTML = `<img src="${product.imageUrl}" alt="Photo de ${product.altTxt}">`;
+    document.getElementById("title").textContent = product.name;
+    document.getElementById("price").textContent = product.price;
+    document.getElementById("description").textContent = product.description;
+
+    // Affichage des options
+    document.getElementById("colors").innerHTML = product.colors.map((color) => `<option value="${color}">${color}</option>`).join("");
+});
+
+/**
+ * Ajoute un produit au panier lors du clic sur le bouton
+ */
+document.getElementById("addToCart").addEventListener("click", () => {
+    const quantity = document.getElementById("quantity").value;
+    const color = document.getElementById("colors").value;
+
+    if (quantity > 0 || quantity <= 100) {
+        addToCart(productId, quantity, color);
+        window.alert("Le produit a bien été ajouté au panier");
+    } else window.alert("Veuillez sélectionner une quantité valide");
+});
+
+/**
+ * Ajoute un produit au panier
+ * @param productId
+ * @param quantity
+ * @param color
+ */
+function addToCart(productId, quantity, color) {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    const product = cart.find((product) => product.id === productId && product.color === color);
+
+    if (product) product.quantity += quantity;
+    else cart.push({ id: productId, quantity, color });
+
+    localStorage.setItem("cart", JSON.stringify(cart));
+}
